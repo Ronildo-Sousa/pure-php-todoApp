779bb830c13660f67be745593318a83a9b8b7060
Ronildo Sousa <sousaronildo400@gmail.com>
2023-03-28 10:33:58 -0300

    add .env component

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
+use Dotenv\Dotenv;
+
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/src/config/routes.php";
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+var_dump($_ENV);
