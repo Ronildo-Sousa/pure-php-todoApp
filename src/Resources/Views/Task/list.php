5442194df5505b8746ef9a251ce79669914163e8
Ronildo Sousa <sousaronildo400@gmail.com>
2023-04-03 14:16:12 -0300

    filter tasks frontend

--- src/Resources/Views/Task/list.php
+++ src/Resources/Views/Task/list.php
@@ -2,6 +2,22 @@
 
 <div class="task-list">
     <h1>Minha lista de tarefas</h1>
+    <form method="get" action="#" class="card search-form">
+        <div class="search-content">
+            <span>Filtrar tarefas:</span>
+            <div>
+                <select name="type" id="type">
+                    <option value="">manutenção</option>
+                </select>
+            </div>
+            <div>
+                <select name="status" id="type">
+                    <option value="">manutenção</option>
+                </select>
+            </div>
+            <button type="submit" class="btn">filtrar</button>
+        </div>
+    </form>
 
     <?php if (count($tasks) == 0) : ?>
         <div class="card">
@@ -11,26 +27,24 @@
 
     <?php foreach ($tasks as $task) : ?>
         <div class="card">
-            <div class="card-header">
-                <h3><?= $task->title; ?></h3>
-                <div>
-                    <span class="tag-span"><?= $task->type; ?></span>
-                    <span class="tag-span"><?= $task->status; ?></span>
+            <div class="task-content">
+                <div class="task-header">
+                    <h3><?= $task->title; ?></h3>
+                    <div>
+                        <span class="tag-span"><?= $task->type; ?></span>
+                        <span class="tag-span"><?= $task->status; ?></span>
+                    </div>
                 </div>
-            </div>
-            <div class="card-content">
-                <div class="task-decription">
-                    <p><?= $task->description; ?></p>
+                <div class="task-body">
+                    <div class="task-decription">
+                        <p><?= $task->description; ?></p>
+                    </div>
+                    <div>
+                        <a href="#" class="btn delete">
+                            apagar
+                        </a>
+                    </div>
                 </div>
-                <div>
-                    <a href="#" class="action edit">
-                        editar
-                    </a>
-                    <a href="#" class="action delete">
-                        apagar
-                    </a>
-                </div>
-
             </div>
         </div>
     <?php endforeach; ?>
